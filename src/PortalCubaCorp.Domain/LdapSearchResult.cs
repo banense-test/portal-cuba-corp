55e9481a5e263d63f91eb5b40abb2247bc651a14
banense-test <iddelacruz@outlook.com>
2026-08-28 12:57:15 +0200

    fix: remove stray box-drawing character in LdapSearchResult.cs
    [rup:3a23563d-edb5-2618-910b-c65447a769e5]

--- src/PortalCubaCorp.Domain/LdapSearchResult.cs
+++ src/PortalCubaCorp.Domain/LdapSearchResult.cs
@@ -24,7 +24,7 @@ public class LdapSearchResult
 
     /// <summary>
     /// Department (department attribute).
-    │   /// </summary>
+    /// </summary>
     public string? Department { get; set; }
 
     /// <summary>
